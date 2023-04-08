1f72f33986140493de27d3bf0cb9792567799492
DESKTOP-2JMG064\igca0 <mmateoguillen@gmail.com>
2023-04-07 23:38:26 -0500

    creacion basica del proyecto: navbar y pagina inicial

---
+++ svelte-prueba/src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script>
+	import '../app.css';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<main class="navegacion">
+	<nav class="navbar navbar-expand-lg bg-body-tertiary ">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/">Navbar</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon" />
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">Features</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/">Pricing</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="/">Disabled</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</main>
+
+<slot />
+
+<style>
+	.container-fluid{
+		padding: 20px;
+		font-size: 20px;
+	}
+</style>
---
+++ svelte-prueba/src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<main class="contenedor">
+	<h1>Este es el inicio de la pagina aqui encontraras la introduccion:</h1>
+	<p>Hola, bienvenido a mi pagina, aqui podras... <br /> tendras la capacidad de...</p>
+
+	<div class="contenedor-input">
+		<h2>Inicia aqui!</h2>
+		<span class="input-conainter">
+			<div class="mb-3">
+				<label for="exampleFormControlInput1" class="form-label">Email address</label>
+				<input
+					type="email"
+					class="form-control"
+					id="exampleFormControlInput1"
+					placeholder="name@example.com"
+				/>
+			</div>
+			<div class="mb-3">
+				<label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+				<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" />
+			</div>
+		</span>
+	</div>
+</main>
+
+<style>
+	h1 {
+		margin: 20px;
+		text-align: center;
+	}
+	p {
+		font-size: 30px;
+		color: rgb(79, 79, 79);
+		text-align: center;
+	}
+	.contenedor {
+		margin-top: 100px;
+	}
+	.contenedor-input {
+		width: 30%;
+		margin-top: 80px;
+        padding: 20px;
+        border-radius: 10px;
+		transform: translateX(50px);
+        transition-property: outline;
+        transition-duration: .4s;
+        outline: 2px dashed rgba(0, 0, 0, 0.218);
+	}
+	.form-control {
+		resize: none;
+	}
+    .contenedor-input:hover{
+        outline: 2px dashed black;
+    }
+    
+</style>
