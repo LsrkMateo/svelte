b181f62c8381c017cb19548ccd79266419392b68
DESKTOP-2JMG064\igca0 <mmateoguillen@gmail.com>
2023-04-08 00:04:00 -0500

    arreglar propiedades a imagen

--- svelte-prueba/src/routes/+page.svelte
+++ svelte-prueba/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <main class="contenedor">
 	<h1>Este es el inicio de la pagina aqui encontraras la introduccion:</h1>
-	<p>Hola, bienvenido a "nombre de la pagina" <br /> El lugar donde podras potenciar tus habilidades personales y compartirlas con otros</p>
+	<p>
+		Hola, bienvenido a "nombre de la pagina" <br /> El lugar donde podras potenciar tus habilidades personales
+		y compartirlas con otros
+	</p>
 
 	<div class="contenedor-input">
-		<h2>Inicia aqui!</h2>
+		<h2>Registrate:</h2>
 		<span class="input-conainter">
 			<div class="mb-3">
 				<label for="exampleFormControlInput1" class="form-label">Email address</label>
@@ -20,6 +23,16 @@
 			</div>
 		</span>
 	</div>
+	<!-- <div class="imagen-contenedor">
+		<img
+			src="https://thumbs.dreamstime.com/b/nuevo-concepto-de-la-tecnolog%C3%ADa-internet-del-negocio-entrenamiento-webinar-conocimiento-las-habilidades-121274023.jpg"
+			alt="tecnologia"
+			class="imagen"
+		/>
+        <div class="lorem">
+            <p>Holaaa Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut ea similique nemo facilis fugit doloribus totam at, sapiente aliquid minus dolores possimus perspiciatis quod ipsam dolor laborum, illum quia. Nihil!</p>
+        </div>
+	</div> -->
 </main>
 
 <style>
@@ -37,19 +50,19 @@
 	}
 	.contenedor-input {
 		width: 30%;
-		margin-top: 80px;
-        padding: 20px;
-        border-radius: 10px;
+		margin-top: 50px;
+		padding: 20px;
+		border-radius: 10px;
 		transform: translateX(50px);
-        transition-property: outline;
-        transition-duration: .4s;
-        outline: 2px dashed rgba(0, 0, 0, 0.218);
+		transition-property: outline;
+		transition-duration: 0.4s;
+		outline: 2px dashed rgba(0, 0, 0, 0.218);
 	}
 	.form-control {
 		resize: none;
 	}
-    .contenedor-input:hover{
-        outline: 2px dashed black;
-    }
-    
+	.contenedor-input:hover {
+		outline: 2px dashed black;
+	}
+   
 </style>
